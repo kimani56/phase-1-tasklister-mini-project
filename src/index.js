fb2f2ec0d2c8ddcb49eebaf2daec5af3cd848ce9
gift kimani <gift.kimani@students.moringaschool.com>
2023-06-29 02:13:23 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ let form = document.querySelector("#create-task-form")
+  form.addEventListener("submit",(e=> {
+    e.preventDefault()
+    buildTodo(e.target.querySelector("#new-task-description").value);
+form.reset()
+  }));
 });
+ function buildTodo(todo){
+   let p = document.createElement('p');
+   let btn = document.createElement("button");
+   btn.addEventListener("click",handleDelete);
+   btn.textContent = 'x'
+   p.textContent =`${todo} `;
+    p.appendChild(btn);
+    console.log(p);
+  document.querySelector('#tasks').appendChild(p);
+ }
+
+ function handleDelete(e){
+  e.target.parentNode.remove();
+ }
